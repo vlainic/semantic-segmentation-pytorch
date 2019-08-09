be9abe406b221f08b3795f231e6cc2ea5c59ff41
Milos Vlainic <41046983+vlainic@users.noreply.github.com>
2019-08-09 07:51:43 +0200

    Update hrnet.py

--- models/hrnet.py
+++ models/hrnet.py
@@ -438,7 +438,7 @@ class HRNetV2(nn.Module):
 
 
 def hrnetv2(pretrained=False, **kwargs):
-    model = HRNetV2(n_class=1000, **kwargs)
+    model = HRNetV2(n_class=2, **kwargs)
     if pretrained:
         model.load_state_dict(load_url(model_urls['hrnetv2']), strict=False)
 
